c8cb9c4f66cbc587ef1054101b71d477b6bf5b92
NightCatSama <642163903@qq.com>
2018-04-19 12:28:35 +0800

    chore(commitlint): add rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type-enum": [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'chore',
+        'perf',
+        'temp'
+      ]
+    ]
+  }
 }
